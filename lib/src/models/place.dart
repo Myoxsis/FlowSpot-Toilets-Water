91804f033308285f12177f02b916f0a8b8156b61
maxime_alain <69240270+Myoxsis@users.noreply.github.com>
2026-05-20 21:27:16 +0200

    Add Place JSON serialization support

--- lib/src/models/place.dart
+++ lib/src/models/place.dart
@@ -41,4 +41,44 @@ class Place {
   String get distanceLabel => distanceMeters < 1000
       ? '${distanceMeters}m'
       : '${(distanceMeters / 1000).toStringAsFixed(1)}km';
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'type': type.name,
+        'distanceMeters': distanceMeters,
+        'address': address,
+        'isFree': isFree,
+        'isOpen': isOpen,
+        'isWheelchairAccessible': isWheelchairAccessible,
+        'cleanlinessScore': cleanlinessScore,
+        'trustScore': trustScore,
+        'verifiedMinutesAgo': verifiedMinutesAgo,
+        'reviewCount': reviewCount,
+        'latitude': latitude,
+        'longitude': longitude,
+        'hasBabyChanging': hasBabyChanging,
+        'isBottleFriendly': isBottleFriendly,
+      };
+
+  factory Place.fromJson(Map<String, dynamic> json) {
+    return Place(
+      id: json['id'] as String? ?? 'unknown',
+      name: json['name'] as String? ?? 'Public spot',
+      type: json['type'] == 'fountain' ? PlaceType.fountain : PlaceType.toilet,
+      distanceMeters: json['distanceMeters'] as int? ?? 0,
+      address: json['address'] as String? ?? 'Cached spot',
+      isFree: json['isFree'] as bool? ?? true,
+      isOpen: json['isOpen'] as bool? ?? true,
+      isWheelchairAccessible: json['isWheelchairAccessible'] as bool? ?? false,
+      cleanlinessScore: (json['cleanlinessScore'] as num?)?.toDouble() ?? 0,
+      trustScore: json['trustScore'] as int? ?? 50,
+      verifiedMinutesAgo: json['verifiedMinutesAgo'] as int? ?? 999,
+      reviewCount: json['reviewCount'] as int? ?? 0,
+      latitude: (json['latitude'] as num?)?.toDouble() ?? 48.8566,
+      longitude: (json['longitude'] as num?)?.toDouble() ?? 2.3522,
+      hasBabyChanging: json['hasBabyChanging'] as bool? ?? false,
+      isBottleFriendly: json['isBottleFriendly'] as bool? ?? false,
+    );
+  }
 }
